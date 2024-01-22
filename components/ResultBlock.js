3858510baa8916a4146870a3cfe65d90256898a4
Mark Jason <136074354+camerastuffdevelopment@users.noreply.github.com>
2024-01-22 12:29:06 +0200

    [C] Fixed some final bugs

--- components/ResultBlock.js
+++ components/ResultBlock.js
@@ -1,7 +1,7 @@
 const ResultBlock = ({ result, words }) => {
   return (
     <div className="bg-white h-auto rounded-3xl">
-      {words.length > 0 ? (
+      {words.trim().length > 0 ? (
         <div
           className={`flex justify-center items-center font-bold font-mono text-4xl ${
             result ? "bg-green-500" : "bg-red-500"
